fafdae2c9e9bfe7a4cfefa16ac88d99aa6ed4f17
jordaneaster <jordaneaster222@icloud.com>
2021-06-23 10:57:13 -0400

    donutmaker purchase functionality

--- donutMaker.js
+++ donutMaker.js
@@ -1,22 +1,63 @@
 class DonutMaker {
- 
- 
   constructor(myDonuts, myAutoClickers, autoClickerCost, myDonutX, donutXCost) {
-    this.myDonuts = myDonuts;
-    this.myAutoClickers = myAutoClickers;
-    this.autoClickerCost = autoClickerCost;
-    this.myDonutX = myDonutX;
-    this.donutXCost = donutXCost;
+    this.myDonuts = 0;
+    this.myAutoClickers = 0;
+    this.autoClickerCost = 33;
+    this.myDonutX = 0;
+    this.donutXCost = 10;
+  }
+  //click add/buy buttons
+  clickDonutButton() {
+    this.myDonuts += 1;
+  }
+  buyAutoclicker() {
+    this.myAutoClickers += 1;
+    this.subtractDonutsToBuyAutoClicker(); 
+  }
+  buyDonutX() {
+    this.myDonutX += 1;
+    this.subtractDonutsToBuyDonutX();
   }
 
-clickDonutButton(){
-  this.myDonuts +=1;
-}
-returnDonuts(){
-  return Math.round(this.myDonuts);
-}
+  //getters
+  returnDonuts() {
+    return Math.round(this.myDonuts);
+  }
+  returnAutoClickers() {
+    return this.myAutoClickers;
+  }
+  returnDonutX() {
+    return this.myDonutX;
+  }
 
+  //clicker calculations-sufficient funds
+  subtractDonutsToBuyAutoClicker() {
+    this.myDonuts -= Math.round(this.autoClickerCost);
+  }
+  subtractDonutsToBuyDonutX(){
+    this.myDonuts -= Math.round(this.donutXCost);
+  }
+  insufficientFunds() {
+    return this.myDonuts < this.autoClickerCost;
+  }
+  insufficientFundsX() {
+    return this.myDonuts < this.donutXCost;
+  }
 
- 
+  autoClickerFundsChecker(){
+    if(this.insufficientFunds()){
+     throw new Error('insufficient donuts to buy auto clicker');
+    } else{
+      this.buyAutoclicker();
+    }
+  }
+  donutXFundsChecker(){
+    if(this.insufficientFunds()){
+     throw new Error('insufficient donuts to buy auto clicker');
+    } else{
+      this.buyDonutX();
+    }
+  }
+
 }
 export default DonutMaker;
--- jsdc/index.js
+++ jsdc/index.js
@@ -40,8 +40,91 @@ const donutClicker = document.querySelector(".donutClicker"),
   donutAmount = document.querySelector(".donutAmount"),
   acAmount = document.querySelector(".acAmount"),
   dxAmount = document.querySelector(".dxAmount");
+
+//donut maker object
 const donutMaker = new DonutMaker();
 
+
+
+//update counts
+function updateDonutCount(){
+  const donutCount = document.querySelector(".donutAmount");
+  donutCount.innerHTML = donutMaker.returnDonuts();
+  checkFundsToBuyAutoClicker();
+  checkFundsToBuyDonutX();
+}
+function updateDonutXCount(){
+  const dxAmount = document.querySelector(".dxAmount");
+  dxAmount.innerHTML = donutMaker.returnDonutX();
+  checkFundsToBuyAutoClicker();
+  checkFundsToBuyDonutX();
+}
+function updateAutoClickerCount(){
+  const dxAmount = document.querySelector(".acAmount");
+  dxAmount.innerHTML = donutMaker.returnAutoClickers();
+  checkFundsToBuyAutoClicker();
+  checkFundsToBuyDonutX();
+}
+function updateAllCounts(){
+  updateDonutCount();
+  updateAutoClickerCount();
+  updateDonutXCount();
+}
+
+
+//click to make donut button
 donutClicker.addEventListener("click", () => {
   donutMaker.clickDonutButton();
+  displayMyDonuts();
+  updateDonutCount();
 });
+
+//click to buy autoclickers
+autoClicker.addEventListener("click", () => {
+  checkFundsToBuyAutoClicker();
+  donutMaker.buyAutoclicker();
+  updateAllCounts();
+  displayMyAutoClickers();
+});
+//click to buy donutX
+donutX.addEventListener("click", () => {
+  checkFundsToBuyDonutX();
+  donutMaker.buyDonutX();
+  updateAllCounts();
+  displayDonutX();
+});
+
+//check funds to buy auto clicker
+function checkFundsToBuyAutoClicker(){
+  const autoClicker = document.querySelector(".buyAutoClicker");
+  if(donutMaker.insufficientFunds()){
+    autoClicker.disabled = true;
+  } else{
+    autoClicker.disabled = false
+  }
+}
+//check funds to buy donut x 
+function checkFundsToBuyDonutX(){
+  const donutX = document.querySelector(".buyDonutX");
+  if(donutMaker.insufficientFundsX()){
+    donutX.disabled = true;
+  } else{
+    donutX.disabled = false
+  }
+}
+//display autoclickers
+function displayMyAutoClickers() {
+  const acAmount = document.querySelector(".acAmount");
+  acAmount.innerHTML = donutMaker.returnAutoClickers();
+}
+//display donut count
+function displayMyDonuts() {
+  const donutAmount = document.querySelector(".donutAmount");
+  donutAmount.innerHTML = donutMaker.returnDonuts();
+}
+
+//display donut x
+function displayDonutX() {
+  const dxAmount = document.querySelector(".dxAmount");
+  dxAmount.innerHTML = donutMaker.returnDonutX();
+}
